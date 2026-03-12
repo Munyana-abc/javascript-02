6ab02f658120ef8922e9a17f251b16d49f08135b
Munyana-abc <favourmunyana08@gmail.com>
2026-03-12 14:44:35 -0700

    bus fare eligibility

---
+++ keke.js
@@ -0,0 +1,16 @@
+let ticketPrice = 500;
+let balance = 700;
+
+if (balance >= ticketPrice) {
+    console.log("User can pay for the ticket.");
+} else {
+    console.log("User cannot pay for the ticket.");
+}
+let age = 15;
+console.log(age) 
+
+if (balance >= ticketPrice && age > 12) {
+    console.log("You can board the bus.");
+} else {
+    console.log("You cannot board the bus.");
+}
